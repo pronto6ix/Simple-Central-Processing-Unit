cbd29d48f5f525125d24193fada0611816e038a3
pronto6ix <121268234+pronto6ix@users.noreply.github.com>
2025-08-08 15:20:23 -0400

    Create latch1.vhd

---
+++ latch1.vhd
@@ -0,0 +1,20 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY latch1 IS
+	port ( A : in std_logic_vector(7 downto 0) ; -- 8-bit A input
+		res, clk : in std_logic ;
+			Q : out std_logic_vector(7 downto 0));  -- 8-bit output
+end latch1;
+
+architecture behavior of latch1 is
+begin
+	process (res, clk)
+	begin
+		if res = '1' then
+			Q <= "00000000" ;
+		elsif (clk'EVENT AND clk = '1') then
+			Q <= A ;
+		end if ;
+	end process ;
+end behavior ;
